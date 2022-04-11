d2e3892e509aa1a97fd369c2ed7fb921f22d4973
Raphael J. Honorato <rph.devel@gmail.com>
2022-04-11 15:01:52 -0300

    Sobre construtor e objetos

--- JDevTreinamentos/src/br/com/javafullstack/orieontacaoObjetos/Aluno.java
+++ JDevTreinamentos/src/br/com/javafullstack/orieontacaoObjetos/Aluno.java
@@ -12,5 +12,26 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
+	
+	/* Podemos criar um construtor para definir valores padrões */
+	
+	/* Cria dados na memoria, sendo padrão do java, mesmo que não crie aqui o java usa um construtor que não vemos */
+	public Aluno() {
+		// TODO Auto-generated constructor stub		
+		
+	}
+	
+	/* Construtor com varialvel nomePadrao, quando essa variavel recebe valor ela passa para a variavel nome */
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	/* Aqui é um construtor que precisa receber nome e idade
+	 * que é recebido no nomePadrao e idadePadrao e depois passado para
+	 * as variáveis nome e idade */
+	public Aluno(String nomePadrao, int idadePadrao ) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
 
 }
---
+++ JDevTreinamentos/src/br/com/javafullstack/orieontacaoObjetos/Main.java
@@ -0,0 +1,19 @@
+package br.com.javafullstack.orieontacaoObjetos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Objeto ainda não existe na memória  */
+		Aluno aluno1;
+		
+		/* Agora temos um objeto real */
+		Aluno aluno2 = new Aluno();
+		
+		/* Aqui to criando o objeto aluno3 passando os parâmetros nome e idade  */
+		Aluno aluno3 = new Aluno("Raphael", 34);
+
+	}
+
+}
