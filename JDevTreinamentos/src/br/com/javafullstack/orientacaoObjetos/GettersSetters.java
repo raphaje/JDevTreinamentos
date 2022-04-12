33c42b73b8abb560e6c2952b747989372aa96d79
Raphael J. Honorato <rph.devel@gmail.com>
2022-04-12 13:51:18 -0300

    Usando os métodos getters and setters

--- JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/GettersSetters.java
+++ JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/GettersSetters.java
@@ -21,5 +21,89 @@ public class GettersSetters {
 	/* Veremos os metodos getters e setters
 	 * SET é para adicionar dados para os atributos
 	 * GET é para recuperar, receber dados de um atributo */
+	
+	/* Recebe dados */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/* Retorna dados */
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
 
 }
--- JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/Main.java
+++ JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/Main.java
@@ -16,10 +16,39 @@ public class Main {
 		aluno2.idade = 34;
 		
 		System.out.println("Nome do aluno é: " + aluno2.nome);
-		System.out.println("Idade do aluno é: " + aluno2.idade);
+		System.out.println("Idade do aluno é: " + aluno2.idade + "\n");
 		
 		/* Aqui to criando o objeto aluno3 passando os parâmetros nome e idade  */
 		Aluno aluno3 = new Aluno("Raphael", 34);
+		
+		
+		/* GETTERS AND SETTERS */
+		
+		/* Criando Objeto, passando parametros e recebendo valor usando o getters e setters */
+		GettersSetters aluno4 = new GettersSetters();
+		aluno4.setNome("Raphael Honorato");
+		aluno4.setIdade(34);
+		aluno4.setDataNascimento("05/07/1987");
+		aluno4.setRegistroGeral("2222222222222");
+		aluno4.setNumeroCpf("33333333333");
+		aluno4.setNomeMae("Raimunda");
+		aluno4.setNomePai("Jamal");
+		aluno4.setDataMatricula("01/01/2022");
+		aluno4.setNomeEscola("Escola 1");
+		aluno4.setSerieMatriculado("Primera série");
+		
+		
+		System.out.println("Usando método getters and setters:");
+		System.out.println("Nome do aluno é: " + aluno4.getNome());
+		System.out.println("Idade do aluno é: " + aluno4.getIdade());
+		System.out.println("Data do Nascimento do aluno é: " + aluno4.getDataNascimento());
+		System.out.println("RG do aluno é: " + aluno4.getRegistroGeral());
+		System.out.println("CPF do aluno é: " + aluno4.getNumeroCpf());
+		System.out.println("Nome da mãe do aluno é: " + aluno4.getNomeMae());
+		System.out.println("Nome do pai do aluno é: " + aluno4.getNomePai());
+		System.out.println("Data da matrícula do aluno é: " + aluno4.getDataMatricula());
+		System.out.println("Nome da escola do aluno é: " + aluno4.getNomeEscola());
+		System.out.println("Serie do aluno é: " + aluno4.getSerieMatriculado());
 
 	}
 
