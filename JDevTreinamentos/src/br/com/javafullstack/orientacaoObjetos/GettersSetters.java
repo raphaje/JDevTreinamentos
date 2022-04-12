357fad84a191bee51a136c4e9a3f9e7a51d7a36b
Raphael J. Honorato <rph.devel@gmail.com>
2022-04-12 11:34:32 -0300

    Getters and Setters

---
+++ JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/GettersSetters.java
@@ -0,0 +1,25 @@
+package br.com.javafullstack.orientacaoObjetos;
+
+public class GettersSetters {
+	
+	/* Esse são os atributos do aluno. 
+	 * Quando não definimos o estado desse atributo
+	 * o java usa por padrão o private,
+	 * podemos usar os valores public, private e protected
+	 * */
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	
+	/* Veremos os metodos getters e setters
+	 * SET é para adicionar dados para os atributos
+	 * GET é para recuperar, receber dados de um atributo */
+
+}
