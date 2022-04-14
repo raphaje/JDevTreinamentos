020a6fbb528f75c11dfd3cfe308d3c62607be885
Raphael J. Honorato <rph.devel@gmail.com>
2022-04-14 11:53:24 -0300

    Entrada de dados do teclado usando JOptionPane e convertendo valores numéricos.

--- JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/Aluno.java
+++ JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/Aluno.java
@@ -18,9 +18,89 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
-	
+		
 	/* Podemos criar um construtor para definir valores padrões */
 	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
 	/* Cria dados na memoria, sendo padrão do java, mesmo que não crie aqui o java usa um construtor que não vemos */
 	public Aluno() {
 		// TODO Auto-generated constructor stub		
---
+++ JDevTreinamentos/src/br/com/javafullstack/orientacaoObjetos/MainEntradaDeDadosTeclado.java
@@ -0,0 +1,32 @@
+package br.com.javafullstack.orientacaoObjetos;
+
+import javax.swing.JOptionPane;
+
+public class MainEntradaDeDadosTeclado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Usando a biblioteca JOptionPane para inserir dados na aplicação */
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno ? ");
+		
+		/* OBS: O JOptionPane é do tipo String, caso seja inserido um número que vai usado em um método do tipo inteiro
+		 * ou para fazer cálculo sempre sera necessário converter para o tipo inteiro
+		 * Nesse exemplo aqui criamos a variável idade do tipo inteiro e já usamos o JOptionPane fazendo
+		 * a conversão do tipo usando o Interger.ParseInt */
+		int idade = (+ Integer.parseInt(JOptionPane.showInputDialog("Qual a idade ?")));
+		
+		/* Criando o objeto */
+		Aluno aluno1 = new Aluno();
+		
+		/* Passando o dado armazenado na varável "nome" para o método setNome do objeto aluno1 */
+		aluno1.setNome(nome);
+		aluno1.setIdade(idade);
+		
+		/* Recuperando o nome do objeto aluno1 usando o método getNome e imprimindo no console. */
+		System.out.println("O nome do aluno é = " + aluno1.getNome());
+		System.out.println("A idade do aluno é = " + aluno1.getIdade());
+
+	}
+
+}
